fba593bc49077961ec96b903ae1da0fb6b04763a
Nikolaus Schuetz <nikolauspschuetz@gmail.com>
2025-01-10 18:37:04 -0500

    fixing up to deploy pypi server to my k8s

--- apply.sh
+++ apply.sh
@@ -21,6 +21,11 @@ do
   f_bust=$(echo "$f" | sed 's/^t8s\./k8s\//')
   echo "Creating $f_bust"
   envsubst < "$f" > "$f_bust"
-dones
+done
 
 kubectl apply -f k8s/
+
+pod_name_expr='.items[] | select(.metadata.labels.app == "pypiserver").metadata.name'
+POD_NAME="$(kubectl get pods --output=json | jq -rc "$pod_name_expr")"
+echo "Deployed Pod: $POD_NAME"
+echo "To forward $PYPI_IMAGE from k8s please run: kubectl port-forward pod/$POD_NAME 8080:80 --address 0.0.0.0"
